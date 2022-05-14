30a39813d1ca294e087a0e4f08a303f003d4d2c0
waiyanmyatoo <waiyan.wy119@gmail.com>
2022-05-14 11:10:06 +0630

    Add user agent in network request

--- services/fetch_html_service.js
+++ services/fetch_html_service.js
@@ -1,16 +1,24 @@
 const rs = require("request");
 
 const get_html = function (url, callback) {
-     rs(url, function (error, response, html) {
+    rs(url, function (error, response, html) {
         callback(error, response, html);
     });
 }
 
-const get_schedules = function (url) {
-    
 
-    rs(baseURL.concat(path.allFootball), (error, response, html) => {
-        
+
+const get_schedules = function (url) {
+
+    var options = {
+        'method': 'GET',
+        'url': baseURL,
+        'headers': {
+            'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+        }
+    };
+    rs(options.concat(path.allFootball), (error, response, html) => {
+
     });
 }
 
